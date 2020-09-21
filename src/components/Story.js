82b95d1c2e7a961c79e364af545b8aeb10ad1fde
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-09-21 11:11:46 +0700

    setup styled components

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,7 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 
-import { getStory, storyUrl } from "../services/hnApi";
+import { getStory } from "../services/hnApi";
+
+import { StoryWrapper } from "../styles/StoryStyles";
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -11,12 +13,12 @@ export const Story = ({ storyId }) => {
   }, []);
 
   return story && story.url ? (
-    <>
+    <StoryWrapper data-testid="story">
       <a href={story.url}>
         <p>{story.title}</p>
       </a>
       By : <p>{story.by}</p>
       Posted : <p>{story.time}</p>
-    </>
+    </StoryWrapper>
   ) : null;
 };
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getStoryIds, getStory } from "../services/hnApi";
+import { getStoryIds } from "../services/hnApi";
 
 import { Story } from "../components/Story";
 
@@ -10,5 +10,12 @@ export const StoriesContainer = () => {
     getStoryIds().then((data) => setStoryIds(data));
   }, []);
 
-  return storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />);
+  return (
+    <>
+      <h1>Hacker News Stories</h1>
+      {storyIds.map((storyId) => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+    </>
+  );
 };
